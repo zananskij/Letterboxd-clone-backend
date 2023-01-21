119eaac507f60f65d5c7e3fe01dc548bb0b6079c
zananskij <zananskij@gmail.com>
2023-01-21 16:22:27 -0500

    asd

--- dist/index.js
+++ dist/index.js
@@ -13,135 +13,196 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+// import cors from "cors"
+// const port = process.env.PORT || 8000
 const axios_1 = __importDefault(require("axios"));
-const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-// const port = 8000
-const port = process.env.PG_PORT || 8000;
-// app.listen(port, () => {
-//   console.log(`listening on port ${port} `)
-// })
-app.listen(port, () => {
-    console.log(`listening on this port ${port} || process.env.PG_PORT`);
-});
-app.use((0, cors_1.default)());
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = process.env.API_KEY;
-const { Client } = require("pg");
-const client = new Client({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    URL: process.env.PG_URL,
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const { Pool } = require("pg");
+const port = parseInt(process.env.PORT || "5432", 10);
+const pool = new Pool({
+    connectionString: `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${port}/${process.env.PG_DATABASE}`,
     ssl: {
         rejectUnauthorized: false,
     },
 });
-client.connect((err) => {
+pool.connect((err) => {
     if (err) {
-        console.error("connection error", err.stack);
+        console.log("Error connecting to the database:", err);
     }
     else {
-        console.log("connected");
+        console.log("Connected to the database");
     }
 });
-const jwt = require("jsonwebtoken");
-app.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // verify
-    const token = req.headers.authorization;
-    // verify
-    const { username, password } = req.body;
-    try {
-        const result = yield client.query("INSERT INTO users(username,password) VALUES($1, $2) RETURNING id", [
-            username,
-            password,
-        ]);
-        const token = jwt.sign({ userId: result.rows[0].id }, process.env.SECRET_KEY, {
-            expiresIn: "24h",
-        });
-        res.status(201).json({ token });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Error creating user" });
-    }
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    jwt.verify(token, process.env.SECRET_KEY);
-}));
-// register
-app.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // Extract the user's information from the request body
-    const { username, password } = req.body;
-    try {
-        // Insert the user into the database
-        const result = yield client.query("INSERT INTO users(username,password) VALUES($1, $2) RETURNING id", [
-            username,
-            password,
-        ]);
-        // Create a new token
-        const token = jwt.sign({ userId: result.rows[0].id }, process.env.SECRET_KEY, {
-            expiresIn: "24h",
-        });
-        // associate the token with the user in the database
-        yield client.query("INSERT INTO tokens (user_id, token) VALUES ($1, $2)", [result.rows[0].id, token]);
-        // Send the token back in the response
-        res.status(201).json({ token });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Error creating user" });
-    }
-}));
-// login
-app.post("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // Extract the user's information from the request body
-    const { username, password } = req.body;
-    try {
-        // Verify that the user's credentials are correct
-        const result = yield client.query("SELECT * FROM users WHERE username=$1 AND password=$2", [username, password]);
-        if (result.rows.length === 0) {
-            return res.status(401).json({ message: "Invalid credentials" });
-        }
-        // Create a new token
-        const token = jwt.sign({ userId: result.rows[0].id }, process.env.SECRET_KEY, {
-            expiresIn: "24h",
-        });
-        // associate the token with the user in the database
-        yield client.query("INSERT INTO tokens (user_id, token) VALUES ($1, $2)", [result.rows[0].id, token]);
-        // Send the token back in the response
-        res.status(200).json({ token });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Error logging in" });
-    }
-}));
-// app.use(
-//   cors({
-//     origin: "http://localhost:8000/test",
-//   })
-// )
-// app.get("/", (req: Request, res: Response) => {
-//   res.send("main page")
+// const { Client } = require("pg")
+// const port = parseInt(process.env.PORT || "5432", 10)
+// const client = new Client({
+//   host: process.env.PG_HOST,
+//   port: port,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
 // })
-app.get("/profile", (req, res) => {
-    res.send("profile page");
+// client.connect()
+// if (client.connected) {
+//   console.log("Connected to the database")
+// } else {
+//   console.log("Error connecting to the database")
+// }
+app.listen(port, () => {
+    console.log(`listening on this port ${port} || process.env.PG_PORT`);
 });
-app.get("/trending", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield axios_1.default.get(requests.fetchTrending);
-    res.json(response.data);
+// const { Client } = require("pg")
+// const client = new Client({
+//   host: process.env.PG_HOST,
+//   port: process.env.PORT,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+//   URL: process.env.PG_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// })
+// client.connect((err: Error) => {
+//   if (err) {
+//     console.error("connection error", err.stack)
+//   } else {
+//     console.log("connected")
+//   }
+// })
+// const port = process.env.PORT || "5432"
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//   next()
+// })
+// app.listen(port, () => {
+//   console.log(`listening on this port ${port} || process.env.PG_PORT`)
+// })
+// v1
+// app.post("/register", async (req, res) => {
+//   // verify
+//   const token = req.headers.authorization
+//   // verify
+//   const { username, password } = req.body
+//   try {
+//     const result = await client.query("INSERT INTO users(username,password) VALUES($1, $2) RETURNING id", [
+//       username,
+//       password,
+//     ])
+//     const token = jwt.sign({ userId: result.rows[0].id }, process.env.SECRET_KEY, {
+//       expiresIn: "24h",
+//     })
+//     res.status(201).json({ token })
+//   } catch (error) {
+//     console.log(error)
+//     res.status(500).json({ message: "Error creating user" })
+//   }
+//   const decoded = jwt.verify(token, process.env.SECRET_KEY)
+//   jwt.verify(token, process.env.SECRET_KEY)
+// })
+// registerrr v2
+// app.post("/register", async (req, res) => {
+// Extract the user's information from the request body
+// const { username, password } = req.body
+// try {
+//   // Insert the user into the database
+//   const result = await client.query("INSERT INTO users(username,password) VALUES($1, $2) RETURNING id", [
+//     username,
+//     password,
+//   ])
+//   // Create a new token
+//   const tokenToSend = jwt.sign({ userId: result.rows[0].id }, process.env.SECRET_KEY, {
+//     expiresIn: "24h",
+//   })
+//   // associate the token with the user in the database
+//   await client.query("INSERT INTO tokens (user_id, token) VALUES ($1, $2)", [result.rows[0].id, tokenToSend])
+//   // Send the token back in the response
+//   res.status(201).json({ tokenToSend })
+// } catch (error) {
+//   console.log(error)
+//   res.status(500).json({ message: "Error creating user" })
+// }
+// })
+// after connected to db
+// after connected to db
+// after connected to db
+app.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { username, password } = req.body;
+        const text = "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *";
+        const values = [username, password];
+        const { rows } = yield pool.query(text, values);
+        res.status(201).json(rows[0]);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Error registering user" });
+    }
 }));
-app.get("/originals", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield axios_1.default.get(requests.fetchNetflixOriginals);
-    res.json(response.data);
-}));
+// after connected to db
+// after connected to db
+// after connected to db
+// after connected to db
+// login v1
+// app.post("/login", async (req, res) => {
+//   // Extract the user's information from the request body
+//   const { username, password } = req.body
+//   try {
+//     // Verify that the user's credentials are correct
+//     const result = await client.query("SELECT * FROM users WHERE username=$1 AND password=$2", [username, password])
+//     if (result.rows.length === 0) {
+//       return res.status(401).json({ message: "Invalid credentials" })
+//     }
+//     // Create a new token
+//     const token = jwt.sign({ userId: result.rows[0].id }, process.env.SECRET_KEY, {
+//       expiresIn: "24h",
+//     })
+//     // associate the token with the user in the database
+//     await client.query("INSERT INTO tokens (user_id, token) VALUES ($1, $2)", [result.rows[0].id, token])
+//     // Send the token back in the response
+//     res.status(200).json({ token })
+//   } catch (error) {
+//     console.log(error)
+//     res.status(500).json({ message: "Error logging in" })
+//   }
+// })
+// login v2
+// app.post("/login", async (req, res) => {
+//   // Extract the user's information from the request body
+//   const { username, password } = req.body
+//   try {
+//     // Verify that the user's credentials are correct
+//     const result = await client.query("SELECT * FROM users WHERE username=$1 AND password=$2", [username, password])
+//     if (result.rows.length === 0) {
+//       return res.status(401).json({ message: "Invalid credentials" })
+//     }
+//     // Create a new token
+//     const tokenToSend = jwt.sign({ userId: result.rows[0].id }, process.env.SECRET_KEY, {
+//       expiresIn: "24h",
+//     })
+//     // associate the token with the user in the database
+//     await client.query("INSERT INTO tokens (user_id, token) VALUES ($1, $2)", [result.rows[0].id, tokenToSend])
+//     // Send the token back in the response
+//     res.status(200).json({ tokenToSend })
+//   } catch (error) {
+//     console.log(error)
+//     res.status(500).json({ message: "Error logging in" })
+//   }
+// })
 const requests = {
     fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`,
@@ -177,16 +238,24 @@ app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         res.status(500).json({ message: "could not fetch data" });
     });
 }));
-// app.get("/testing", (req, res) => {
-//   res.json({ message: "Hello from the backend!" })
+// app.use(
+//   cors({
+//     origin: "http://localhost:8000/test",
+//   })
+// )
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("main page")
 // })
-// const [trendingNow, netflixOriginals] = await Promise.all([
-//   fetch(requests.fetchTrending).then((res) => res.json()),
-//   fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
-// ])
-// return {
-//   props: {
-//     trendingNow: trendingNow.results,
-//     netflixOriginals: netflixOriginals.results,
-//   },
-// }
+// just removed
+app.get("/profile", (req, res) => {
+    res.send("profile page");
+});
+app.get("/trending", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield axios_1.default.get(requests.fetchTrending);
+    res.json(response.data);
+}));
+app.get("/originals", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield axios_1.default.get(requests.fetchNetflixOriginals);
+    res.json(response.data);
+}));
+// just removed
