593ae7f858f9ae077e7a48c54833357a8807e44b
zananskij <zananskij@gmail.com>
2023-07-20 21:31:48 -0400

    db tested

--- db-test.js
+++ db-test.js
@@ -1,19 +1,19 @@
-// require("dotenv").config()
-// const { Pool } = require("pg")
+require("dotenv").config()
+const { Pool } = require("pg")
 
-// const pool = new Pool({
-//   host: process.env.PG_HOST,
-//   port: process.env.PG_PORT,
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-//   database: process.env.PG_DATABASE,
-// })
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+})
 
-// pool.query("SELECT NOW()", (err, res) => {
-//   if (err) {
-//     console.log("Error executing query", err.stack)
-//   } else {
-//     console.log("Response:", res.rows[0])
-//   }
-//   pool.end()
-// })
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.log("Error executing query", err.stack)
+  } else {
+    console.log("Response:", res.rows[0])
+  }
+  pool.end()
+})
